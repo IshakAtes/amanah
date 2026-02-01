5fb6185cbbca5b36fd8d71393eb1610bfbb3ce8d
Ishak ates <ishakfeuer@gmail.com>
2026-02-02 00:21:42 +0100

    linking focus buttons

--- src/app/focus/focus.ts
+++ src/app/focus/focus.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-focus',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './focus.html',
   styleUrl: './focus.scss',
 })
